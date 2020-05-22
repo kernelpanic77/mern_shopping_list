c0797fbd2d08e9a87bda48de72b26f5ed996a8ce
Ishan Shanware <Ishan.Shanware@iiitb.org>
2020-05-22 16:45:01 +0530

    mobile-view try

--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,7 @@ const middleware = [thunk]; //default exported modules redux-thunk
 
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;  //export default store
