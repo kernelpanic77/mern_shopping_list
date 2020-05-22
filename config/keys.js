bea0e6e139caba6ae7d1d7a0598be35c43b7405d
Ishan Shanware <42925218+MegaMind77-coder@users.noreply.github.com>
2020-05-22 19:38:11 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ishanware_77:iamgamer7777@cluster0-425ud.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: '<your mongoURI goes here>',
 }
